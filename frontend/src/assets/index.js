1971c3b5d7f02a2d71aaee92a8266ebad8de8a0a
Saksham Gambhir <saksham2211gambhir@gmail.com>
2024-09-14 12:12:45 +0530

    optimized image load layout

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,7 +1,6 @@
 // import logo from "./sakshxm08.png";
 import saksham from "./profile-pic (1).png";
-import bg from "./purple-bokeh-digital-business-background.jpg";
 
 const logo =
   "https://s3.ap-south-1.amazonaws.com/assets.sakshxm08.in/sakshxm08-logo-transparent";
-export { logo, saksham, bg };
+export { logo, saksham };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,34 @@
 import { Link } from "react-router-dom";
+import { useEffect, useRef } from "react";
 import { logo, saksham } from "../assets";
 
+// Replace with your CloudFront URL if you set it up
+
 const Navbar = () => {
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = logo;
+
+    if (imgRef.current) {
+      imgRef.current.src = img.src;
+    }
+  }, []);
+
   return (
-    <div className="flex items-center justify-between  w-screen py-6 px-10 relative">
+    <div className="flex items-center justify-between w-screen py-6 px-10 relative">
       <div className="flex items-center gap-4 group select-none">
-        <img
-          src={logo}
-          alt="logo"
-          className="h-16 group-hover:scale-110 transition-all group-active:scale-100 invert"
-        />
+        <div className="w-16 h-16 flex items-center justify-center">
+          {" "}
+          {/* Placeholder color */}
+          <img
+            ref={imgRef}
+            alt="logo"
+            loading="eager"
+            className="h-16 w-16 object-contain group-hover:scale-110 transition-all group-active:scale-100 invert"
+          />
+        </div>
         <div className="flex flex-col cursor-pointer">
           <div className="text-2xl font-extrabold group-hover:text-yellow-400 transition-all group-active:text-yellow-500">
             sakshxm08
@@ -27,8 +46,8 @@ const Navbar = () => {
         >
           Add Project
         </Link>
-        <button className="h-fit flex items-center justify-center hover:border-emerald-300/30 active:border-emerald-600/30 transition-all border-transparent border-8 rounded-full aspect-square ">
-          <img src={saksham} alt="user" className=" h-14" />
+        <button className="h-14 flex items-center justify-center hover:ring-emerald-300/30 active:ring-emerald-600/30 transition-all ring-transparent ring-8 rounded-full aspect-square ">
+          <img src={saksham} alt="user" className=" h-14 rounded-full" />
         </button>
       </div>
     </div>
